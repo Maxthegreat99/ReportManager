c8971148ad7d9abe2f4e8df92776be32d2595ca3
Maxthegreat99 <100855415+Maxthegreat99@users.noreply.github.com>
2023-07-31 16:30:35 -0400

    fix version

--- ReportManager/ReportManager.cs
+++ ReportManager/ReportManager.cs
@@ -15,7 +15,7 @@ namespace ReportManager
             => "ReportManager";
 
         public override Version Version
-            => new Version(2, 0);
+            => new Version(2, 1);
 
         public override string Author
             => "Rozen4334, updated by csmir + RenderBr + Maxthegreat99";
